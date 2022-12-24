698f9f878644d59e97e1a62333f675b489602a43
dev-services@shawnpatoka.com <dev-services@shawnpatoka.com>
2022-12-24 09:00:28 -0600

    uncommented

--- rename.py
+++ rename.py
@@ -4,11 +4,11 @@ STARTS_WITH = "Rick.and.Morty."
 FORMATTED_NAME = "rick_and_morty_"
 
 for file in os.listdir("./"):
-    print(file)
-    # if file.startswith(STARTS_WITH):
-    #     old_name = os.path.join("./", file)
-    #     new_name = old_name.replace(STARTS_WITH,FORMATTED_NAME).replace(".HDTV.x264","")
-    #     os.rename(old_name,new_name)
-    #     print(file)
+    # print(file)
+    if file.startswith(STARTS_WITH):
+        old_name = os.path.join("./", file)
+        new_name = old_name.replace(STARTS_WITH,FORMATTED_NAME).replace(".HDTV.x264","")
+        os.rename(old_name,new_name)
+        print(file)
     
 
